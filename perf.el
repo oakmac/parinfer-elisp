9e968ef4ae2c35b1527217a22f5584b35d1dc5e6
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-06-17 07:49:43 +0900

    tests: try 30 runs to be more reliable

--- perf.el
+++ perf.el
@@ -13,14 +13,14 @@
   "Print the output of benchmark-run."
   (message "  %s: %fs (w/ %d garbage collects in %fs)"
     mode
-    (/ (nth 0 times) 5.0)
-    (/ (nth 1 times) 5.0)
-    (/ (nth 2 times) 5.0)))
+    (/ (nth 0 times) 30.0)
+    (/ (nth 1 times) 30.0)
+    (/ (nth 2 times) 30.0)))
 
 (let* ((filename "tests/really_long_file")
        (text (get-string-from-file filename))
-       (indent-times (benchmark-run 5 (parinferlib-indent-mode text)))
-       (paren-times (benchmark-run 5 (parinferlib-paren-mode text))))
-  (message "\nAverage time to process '%s' (across 5 runs)" filename)
+       (indent-times (benchmark-run 30 (parinferlib-indent-mode text)))
+       (paren-times (benchmark-run 30 (parinferlib-paren-mode text))))
+  (message "\nAverage time to process '%s' (across 30 runs)" filename)
   (print-times "indent-mode" indent-times)
   (print-times " paren-mode" paren-times))
